6cc3a121e65be20b187e71ff1f1484c9a171d96b
Daniel Shitrit <yael_b4@yahoo.com>
2019-03-13 12:59:46 +0200

    DataTemplateSelectorSample

--- DataTemplateSelectorSample/MainWindow.xaml.cs
+++ DataTemplateSelectorSample/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace DataTemplateSelectorSample
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
         public MainWindow()
         {
