85b008660ad1a069b83d91ab27554388ffe47e7b
Daniel Shitrit <yael_b4@yahoo.com>
2019-03-13 12:51:22 +0200

    Add project files.

---
+++ DataTemplateSelectorSample/AlternateTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DataTemplateSelectorSample
+{
+    public class AlternateTemplateSelector : DataTemplateSelector
+    {
+        public string OddTemplate { get; set; }
+        public string EvenTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return (DataTemplate)((FrameworkElement)container).FindResource(
+                (int)item % 2 == 0 ? EvenTemplate : OddTemplate);
+        }
+    }
+}
